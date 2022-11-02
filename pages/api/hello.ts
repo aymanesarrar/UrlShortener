eae1d761b4b3ce6095f6b1655f568df47d267d1e
aymane <aymensarrar1@gmail.com>
2022-11-02 17:04:22 +0100

    init project w/ prisma and nextjs 13

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../db/prisma";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { url } = req.body;
+  const data = await prisma.shortLink.create({
+    data: {
+      url: url,
+      slug: "test",
+    },
+  });
+  console.log(data);
+  res.status(200).json({ name: "John Doe" });
 }
