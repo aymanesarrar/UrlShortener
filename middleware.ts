e5c814a159a29e55f491ea1ab78584e1ab625cd9
aymane <aymensarrar1@gmail.com>
2022-11-06 13:43:01 +0100

    feat(add tailwind, fix middleware): adding tailwind to the project and making the middleware only what comes after the /

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,27 @@
+import { NextApiRequest } from "next";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
-  if (req.nextUrl.pathname.startsWith("/api/getUrl")) {
-    console.log("returning early");
-    return;
-  }
-  const fullPath = req.headers.get("Referer")?.split("/").at(-1);
+  if (req.nextUrl.pathname.split("/")[1].length !== 0) {
+    if (req.nextUrl.pathname.startsWith("/api/getUrl")) {
+      console.log("returning early");
+      return;
+    }
+    const fullPath = req.nextUrl.pathname.split("/")[1];
 
-  const data = await (
-    await fetch(`${req.nextUrl.origin}/api/${fullPath}`)
-  ).json();
-
-  if (typeof data === "string") return NextResponse.redirect(data);
-  else {
-    const { msg } = data;
-    if (msg === "URL not found")
-      return NextResponse.redirect(req.nextUrl.origin);
+    const data = await (
+      await fetch(`${req.nextUrl.origin}/api/${fullPath}`)
+    ).json();
+    if (typeof data === "string") return NextResponse.redirect(data);
+    else {
+      const { msg } = data;
+      console.log(msg);
+      if (msg === "URL not found")
+        return NextResponse.redirect(req.nextUrl.origin);
+    }
   }
 };
 export default middleware;
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico|mockServiceWorker.js).*)"],
+};
--- pages/api/[slug].ts
+++ pages/api/[slug].ts
@@ -3,12 +3,14 @@ import prisma from "../db/prisma";
 
 const getSlug = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
+
   if (typeof slug === "string") {
     const data = await prisma.shortLink.findFirst({
       where: {
         slug: slug,
       },
     });
+
     if (data) {
       const { url } = data;
       if (url.length !== 0) {
